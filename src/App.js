80155cb01cb56baf6652cd9951e19ee9bbbf0338
ShivaRK <shivarkcodes@gmail.com>
2024-06-08 21:11:24 +0530

    fix:footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {  Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Play from "./components/Play";
 import Team from "./components/Team";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/play" element={<Play/>} />
         <Route path="/team" element={<Team/>} />
       </Routes>
+      <Footer/>
     {/* </div> */}
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <div className="flex text-center font-happyMonkey items-center justify-center bg-blue-300 p-3 text-black">
+                <h3 className='font-bold text-lg'>Made by <Link className='underline' to={'https://github.com/ShivaRK17'}>ShivaRK17</Link></h3>
+            </div>
+        </>
+    )
 }
 
 export default Footer
